c32415a588a431342e68fd608f77931ad767038f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-16 09:00:21 -0800

    Update Stringing-me-along.js

--- Stringing-me-along.js
+++ Stringing-me-along.js
@@ -12,3 +12,10 @@ For example:
 
 
 // Solution
+
+function createMessage(str) {
+  return function(next) {
+    if (next === undefined) {return str;}
+      return createMessage(str + " " + next);
+  }
+}
